0807e7d49181bd490894867d5a31bc7dbb3833d7
Eachin <EachinChung@gmail.com>
2022-04-09 01:24:54 +0800

    feat: 聚合 error 能力

---
+++ aggregate.go
@@ -0,0 +1,169 @@
+//go:build go1.13
+// +build go1.13
+
+package errors
+
+//goland:noinspection SpellCheckingInspection
+import (
+	mapset "github.com/deckarep/golang-set"
+)
+
+// Aggregate 表示包含多个错误的对象，但不一定具有单一的语义含义。
+// Aggregate 可以与 errors.Is() 一起使用，以检查特定错误类型的发生。
+// errors.As() 不支持，因为调用者可能关心与给定类型匹配的潜在多个特定错误。
+type Aggregate interface {
+	error
+	Errors() []error
+	Is(error) bool
+}
+
+// NewAggregate 将 errList 转换为 Aggregate，Aggregate 本身就是 errors 接口的实现。
+// 如果 slice 为空，则返回 nil。
+// 它将检查输入 errList 的任何元素是否为 nil，以避免调用 Error() 时出现 nil panic。
+func NewAggregate(errList ...error) Aggregate {
+	if len(errList) == 0 {
+		return nil
+	}
+	// 确保 errList 不包含 nil
+	var errs []error
+	for _, e := range errList {
+		if e != nil {
+			errs = append(errs, e)
+		}
+	}
+	if len(errs) == 0 {
+		return nil
+	}
+	return aggregate(errs)
+}
+
+// aggregate 实现了 error 与 Aggregate 接口。
+type aggregate []error
+
+// Error error 接口的一部分
+func (agg aggregate) Error() string {
+	if len(agg) == 0 {
+		panic("error slice is empty")
+	}
+	if len(agg) == 1 {
+		return agg[0].Error()
+	}
+
+	var result string
+	// 不需要线程安全的 set
+	seenErrs := mapset.NewThreadUnsafeSet()
+
+	agg.visit(func(err error) bool {
+		msg := err.Error()
+		if seenErrs.Contains(msg) {
+			return false
+		}
+		seenErrs.Add(msg)
+		if seenErrs.Cardinality() > 1 {
+			result += ", "
+		}
+		result += msg
+		return false
+	})
+	if seenErrs.Cardinality() == 1 {
+		return result
+	}
+	return "[" + result + "]"
+}
+
+func (agg aggregate) Is(target error) bool {
+	return agg.visit(func(err error) bool {
+		return Is(err, target)
+	})
+}
+
+func (agg aggregate) visit(f func(err error) bool) bool {
+	for _, err := range agg {
+		switch err := err.(type) {
+		case aggregate:
+			if match := err.visit(f); match {
+				return match
+			}
+		case Aggregate:
+			for _, nestedErr := range err.Errors() {
+				if match := f(nestedErr); match {
+					return match
+				}
+			}
+		default:
+			if match := f(err); match {
+				return match
+			}
+		}
+	}
+
+	return false
+}
+
+// Errors 为 Aggregate 的一部分。
+func (agg aggregate) Errors() []error { return agg }
+
+// Matcher 用于匹配 errors。如果 errors 匹配，则返回true。
+type Matcher func(error) bool
+
+// FilterOut 从输入错误中删除与 Matcher 匹配的错误。
+// 如果输入是非 Aggregate error，则仅测试该错误。
+// 如果输入 Aggregate error，错误列表将被递归处理。
+//
+// 例如，这可以用于从错误列表中删除已知的错误 (例如 io.EOF 或 os.PathNotFound )。
+func FilterOut(err error, fns ...Matcher) error {
+	if err == nil {
+		return nil
+	}
+	if agg, ok := err.(Aggregate); ok {
+		return NewAggregate(filterErrors(agg.Errors(), fns...)...)
+	}
+	if !matchesError(err, fns...) {
+		return err
+	}
+	return nil
+}
+
+// matchesError 如果有 Matcher 返回 true，则返回 true
+func matchesError(err error, fns ...Matcher) bool {
+	for _, fn := range fns {
+		if fn(err) {
+			return true
+		}
+	}
+	return false
+}
+
+// filterErrors 返回所有 fns 返回false 的任何 error (或嵌套错误，如果列表包含嵌套错误)。
+// 如果没有 error，则返回 nil。副作用会使所有嵌套的切片变平
+func filterErrors(list []error, fns ...Matcher) []error {
+	var result []error
+	for _, err := range list {
+		r := FilterOut(err, fns...)
+		if r != nil {
+			result = append(result, r)
+		}
+	}
+	return result
+}
+
+// Flatten 将可能嵌套 Aggregate 的 Aggregate 全部递归地压平为一个 Aggregate。
+func Flatten(agg Aggregate) Aggregate {
+	var result []error
+	if agg == nil {
+		return nil
+	}
+	for _, err := range agg.Errors() {
+		if a, ok := err.(Aggregate); ok {
+			r := Flatten(a)
+			if r != nil {
+				result = append(result, r.Errors()...)
+			}
+		} else {
+			if err != nil {
+				result = append(result, err)
+			}
+		}
+	}
+	return NewAggregate(result...)
+}
---
+++ aggregate_test.go
@@ -0,0 +1,304 @@
+//go:build go1.13
+// +build go1.13
+
+package errors
+
+import (
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewAggregate(t *testing.T) {
+	type args struct {
+		errList []error
+	}
+	tests := []struct {
+		name string
+		args args
+		want Aggregate
+	}{
+		{
+			name: "error list is empty",
+			args: args{
+				errList: []error{},
+			},
+			want: nil,
+		},
+		{
+			name: "error is nil",
+			args: args{
+				errList: []error{nil, nil},
+			},
+			want: nil,
+		},
+		{
+			name: "errList is nil",
+			args: args{
+				errList: nil,
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, NewAggregate(tt.args.errList...), "NewAggregate(%v)", tt.args.errList)
+		})
+	}
+}
+
+func Test_aggregate_Error(t *testing.T) {
+	tests := []struct {
+		name string
+		agg  Aggregate
+		want string
+	}{
+		{
+			name: "normal",
+			agg:  NewAggregate(New("err")),
+			want: "err",
+		},
+		{
+			name: "multiple errors",
+			agg:  NewAggregate(New("err-1"), New("err-2")),
+			want: "[err-1, err-2]",
+		},
+		{
+			name: "multiple errors, there are duplicate errors",
+			agg:  NewAggregate(New("err-1"), New("err-1")),
+			want: "err-1",
+		},
+		{
+			name: "multiple errors, there are duplicate errors",
+			agg:  NewAggregate(New("err-1"), New("err-2"), New("err-2")),
+			want: "[err-1, err-2]",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, tt.agg.Error(), "Error()")
+		})
+	}
+}
+
+func Test_aggregate_Error_panic(t *testing.T) {
+	tests := []struct {
+		name string
+		agg  aggregate
+	}{
+		{
+			name: "panic",
+			agg:  nil,
+		},
+		{
+			name: "panic",
+			agg:  aggregate{},
+		},
+		{
+			name: "panic",
+			agg:  aggregate([]error{}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Panics(t, func() { _ = tt.agg.Error() })
+		})
+	}
+}
+
+type mockAggregate []error
+
+func (m mockAggregate) Error() string {
+	return "mockAggregate"
+}
+
+func (m mockAggregate) Errors() []error {
+	return m
+}
+
+func (m mockAggregate) Is(target error) bool {
+	return m.visit(func(err error) bool {
+		return Is(err, target)
+	})
+}
+
+func (m mockAggregate) visit(f func(err error) bool) bool {
+	for _, err := range m {
+		switch err := err.(type) {
+		case Aggregate:
+			for _, nestedErr := range err.Errors() {
+				if match := f(nestedErr); match {
+					return match
+				}
+			}
+		default:
+			if match := f(err); match {
+				return match
+			}
+		}
+	}
+
+	return false
+}
+
+var _ Aggregate = mockAggregate{}
+
+func Test_aggregate_Is(t *testing.T) {
+	err1 := New("err-1")
+	err2 := New("err-2")
+	err3 := New("err-3")
+	type args struct {
+		target error
+	}
+	tests := []struct {
+		name string
+		agg  aggregate
+		args args
+		want bool
+	}{
+		{
+			name: "normal",
+			agg:  aggregate{err1, err2, err3},
+			args: args{
+				target: err3,
+			},
+			want: true,
+		},
+		{
+			name: "normal",
+			agg:  aggregate{err1, aggregate{err2, err3}},
+			args: args{
+				target: err3,
+			},
+			want: true,
+		},
+		{
+			name: "normal",
+			agg:  aggregate{err1, mockAggregate{err2, err3}},
+			args: args{
+				target: err3,
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, tt.agg.Is(tt.args.target), "Is(%v)", tt.args.target)
+		})
+	}
+}
+
+func Test_aggregate_Errors(t *testing.T) {
+	errs := []error{loadConfig()}
+	tests := []struct {
+		name string
+		agg  aggregate
+		want []error
+	}{
+		{
+			name: "normal",
+			agg:  aggregate(errs),
+			want: errs,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, tt.agg.Errors(), "Errors()")
+		})
+	}
+}
+
+func TestFilterOut(t *testing.T) {
+	type args struct {
+		err error
+		fns []Matcher
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name: "normal",
+			args: args{
+				err: NewAggregate(io.EOF, loadConfig()),
+				fns: []Matcher{
+					func(err error) bool {
+						return Is(err, io.EOF)
+					},
+				},
+			},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				assert.NotNil(t, err)
+				assert.Equal(t, err.Error(), "service configuration could not be loaded")
+				return true
+			},
+		},
+		{
+			name: "err is nil",
+			args: args{
+				err: nil,
+				fns: nil,
+			},
+			wantErr: func(t assert.TestingT, err error, i ...interface{}) bool {
+				assert.Nil(t, err)
+				return true
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.wantErr(t, FilterOut(tt.args.err, tt.args.fns...), fmt.Sprintf("FilterOut(%v, ...Matcher)", tt.args.err))
+		})
+	}
+}
+
+func TestFlatten(t *testing.T) {
+	type args struct {
+		agg Aggregate
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "normal",
+			args: args{
+				agg: NewAggregate(NewAggregate(io.EOF, loadConfig())),
+			},
+			want: "[EOF, service configuration could not be loaded]",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, Flatten(tt.args.agg).Error(), "Flatten(%v)", tt.args.agg)
+		})
+	}
+}
+
+func TestFlatten_nil(t *testing.T) {
+	type args struct {
+		agg Aggregate
+	}
+	tests := []struct {
+		name string
+		args args
+		want error
+	}{
+		{
+			name: "normal",
+			args: args{
+				agg: nil,
+			},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, Flatten(tt.args.agg), "Flatten(%v)", tt.args.agg)
+		})
+	}
+}
